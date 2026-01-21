6edad16b6c9eeda7db95b08fac23443953e2b234
Life4Death <santiagodrm@gmail.com>
2026-01-21 13:05:14 +0100

    feat(lists): Adds description property
    It adds the new description property for the Lists Schema, Types and also implement the needed code for the service

--- src/schemas/list.schema.ts
+++ src/schemas/list.schema.ts
@@ -3,12 +3,14 @@ import { colorHexSchema } from "./common.js";
 
 export const createListSchema = z.object({
   title: z.string().min(1).max(50),
+  description: z.string().max(50).optional(),
   color: colorHexSchema.default("#000000"),
 });
 
 export const updateListSchema = z
   .object({
     title: z.string().min(1).max(50).optional(),
+    description: z.string().max(50).optional(),
     color: colorHexSchema.optional(),
   })
   .refine((data) => Object.keys(data).length > 0, {
